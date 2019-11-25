9da5ae544b04deea1ed7547dd237f1d82b5e7865
Kevin Turcumani <kevinjaja98@gmail.com>
2019-11-26 10:01:22 +1100

    Initial commit

---
+++ Assets/Scripts/ShootBullets.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ShootBullets : NetworkBehaviour
+{
+
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    [SerializeField]
+    private float bulletSpeed;
+
+    void Update()
+    {
+        if (this.isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
+        {
+            this.CmdShoot(transform.position);
+        }
+    }
+
+    // Commands - Only run on Server
+    // Rpc (Remote Procedure Call) - Runs on Client but called from Server
+
+    [ClientRpc]
+    void RpcClientShot(Vector3 position)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, position, Quaternion.identity);
+        Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.up * bulletSpeed;
+        Destroy(bullet, 1.0f);
+    }
+
+    [Command]
+    void CmdShoot(Vector3 position)
+    {
+        // Tell the client to spawn a bullet
+        RpcClientShot(position);
+    }
+}
